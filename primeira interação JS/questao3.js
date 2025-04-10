163ea279f18fa34ec85dd46c3459b0d43ccda043
Mateus Viana <mvsc3458@gmail.com>
2025-04-09 21:01:48 -0300

    questao 3

---
+++ primeira interação JS/questao3.js
@@ -0,0 +1,15 @@
+let nota = 9.9 ;
+if (nota >= 7 && nota <= 10) {
+  console.log("Aprovado!");
+}
+else if (nota >= 5 && nota < 7) {
+  console.log("Recuperação!")
+}
+
+else if (nota < 5 && nota >= 0) {
+  console.log("Reprovado!")
+}
+
+else {
+  console.log("Nem a nota sabe digitar, burrão!")
+}
